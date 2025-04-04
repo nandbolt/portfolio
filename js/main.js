cfd6fd7264d473e7adde428d25d14f53c816d3cb
nandbolt <alexwilson5528@gmail.com>
2025-04-03 20:07:42 -0500

    containers

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@
 const greetings = ["Hello there!", "Welcome!", "Good day!", "Hi there!"];
 const greetingIdx = Math.floor(Math.random() * greetings.length);
 const greeting = greetings[greetingIdx];
-document.getElementById("greeting").innerHTML = greeting;
+document.getElementById("greeting").innerHTML = greeting + " " + document.getElementById("greeting").innerHTML;
